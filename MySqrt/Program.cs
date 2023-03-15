ea71b2398f258864dd9b338c86d53dd0cadce02a
Mika Mirzanasiri <mika-mm@hotmail.com>
2023-03-15 17:47:17 +0000

    Add project files.

---
+++ MySqrt/Program.cs
@@ -0,0 +1,26 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(Solution.MySqrt(2147483647));
+    }
+}
+public class Solution
+{
+
+    public static int MySqrt(int x)
+    {
+        double sqr(double x)
+        {
+            return x * x;
+        }
+
+
+        for (int i = 0; i < int.MaxValue; i++)
+        {
+            if (sqr(i) == x) return i;
+            else if (sqr(i) > x) return i - 1;
+        }
+        return 0;
+    }
+}
